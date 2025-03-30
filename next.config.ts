33de65679a454d889c62423a4bec53f83d0103aa
Bhavin <pandya.b@media.net>
2025-03-30 16:34:37 +0530

    feat: strapi and next exxample demo worked

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+
+	// Allow images from localhost
+	images: {
+		domains: ['localhost'],
+	},
 };
 
 export default nextConfig;
